e601ff39f4970221cb61ae1327e340459d76b13a
k-k-github <133872850+k-k-github@users.noreply.github.com>
2023-10-12 15:55:17 +0900

    Update view.py

--- simulator/src/view.py
+++ simulator/src/view.py
@@ -43,16 +43,16 @@ def drawField(canvas, field, x, y, x0, length):
 
 def main():
     root = tk.Tk()
-    root.geometry("600x700")
-    canvas = tk.Canvas(root, width=600, height=700, bg="#eee")
+    root.geometry("850x950")
+    canvas = tk.Canvas(root, width=900, height=1000, bg="#eee")
     canvas.place(x=0, y=0)
     def update():
         global data, finishBool
         canvas.delete("all")
         if data is not None:
             board = data[0].board
-            length = 500//board.height
-            x0 = (600-board.height*length)/2
+            length = 750//board.height
+            x0 = (850-board.height*length)/2
             for y, row in enumerate(board.all):
                 for x, field in enumerate(row):
                     drawField(canvas, field, x, y, x0, length)
@@ -89,24 +89,24 @@ def main():
                 canvas.create_line(*args, fill = "SlateBlue2",
                                    activewidth = 7, width = 3, arrow=tk.LAST)
 
-            x1, y1 = 600-x0+4, 50+board.width*length+4
+            x1, y1 = 850-x0+4, 50+board.width*length+4
             canvas.create_line(x0-4, 46, x1, 46, x1, y1, x0-4, y1, x0-4, 46,
                                fill = "#ddd", width = 4)
             if len(data) == 2:
-                canvas.create_text(300, 620,
+                canvas.create_text(425, 880,
                     text=f"{data[1]} vs {data[0].other}")
                 points = board.calcPoint()
-                canvas.create_text(300, 640,
+                canvas.create_text(425, 900,
                     text=f"{points[0][0]} - {points[1][0]}")
-                canvas.create_text(300, 660,
+                canvas.create_text(425, 920,
                     text=f"{data[0].turns} turns  "
                          f"{data[0].turnTime} seconds   turn {data[0].turn}")
             if len(data) == 4:
-                canvas.create_text(300, 620, text=f"{data[1]} vs {data[2]}")
+                canvas.create_text(425, 880, text=f"{data[1]} vs {data[2]}")
                 points = board.calcPoint()
-                canvas.create_text(300, 640,
+                canvas.create_text(425, 900,
                     text=f"{points[0][0]} - {points[1][0]}")
-                canvas.create_text(300, 660,
+                canvas.create_text(425, 920,
                     text=f"{data[3][0]}   {data[3][1]} turns  "
                          f"{data[3][2]} seconds   turn {data[0].turn}")
         if finishBool: root.destroy()
